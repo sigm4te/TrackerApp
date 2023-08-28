9f80ea456af9558bda260e6b72b2b76e6f03caea
Konstantin <57114608+z18s@users.noreply.github.com>
2023-08-28 23:34:17 +0300

    add: request location permission

--- app/src/main/java/com/example/trackerapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/trackerapp/ui/main/MainFragment.kt
@@ -1,9 +1,11 @@
 package com.example.trackerapp.ui.main
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.trackerapp.databinding.FragmentMainBinding
@@ -36,6 +38,8 @@ class MainFragment : Fragment() {
     }
 
     private fun init() {
+        requestLocationPermission()
+
         map = binding.map
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setBuiltInZoomControls(true)
@@ -45,6 +49,19 @@ class MainFragment : Fragment() {
         val startPoint = GeoPoint(48.8583, 2.2944)
         mapController.setZoom(9.5)
         mapController.setCenter(startPoint)
+    }
+
+    private fun requestLocationPermission() {
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {}
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {}
+                else -> {}
+            }
+        }.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
     }
 
     override fun onPause() {
